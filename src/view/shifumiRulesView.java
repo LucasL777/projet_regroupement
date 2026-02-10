29271183b18d81d3a0178b00a0420e30275c0493
Lucas <lucas.laulanet08@gmail.com>
2026-02-10 15:06:24 +0100

    devine le nombre rules

---
+++ src/view/shifumiRulesView.java
@@ -0,0 +1,89 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class shifumiRulesView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	
+	
+	public shifumiRulesView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 665, 429);
+		setVisible(true);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Start");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				shifumiView frame = new shifumiView();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnNewButton.setBounds(275, 329, 108, 39);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Shifumi");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setBounds(215, 10, 220, 67);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel2 = new JLabel("Nb joueur : 2 (joueur + ordinateur)");
+		lblNewLabel2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel2.setBounds(170, 75, 342, 39);
+		contentPane.add(lblNewLabel2);
+		
+		JLabel lblNewLabel3 = new JLabel("Chaque joueur choisi simultanément entre pierre feuille et ciseaux :");
+		lblNewLabel3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel3.setBounds(28, 124, 599, 53);
+		contentPane.add(lblNewLabel3);
+		
+		JLabel lblNewLabel4 = new JLabel(" - La pierre bat le ciseau.");
+		lblNewLabel4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel4.setBounds(65, 171, 509, 53);
+		contentPane.add(lblNewLabel4);
+		
+		JLabel lblNewLabel5 = new JLabel(" -  Le ciseau bat la feuille.");
+		lblNewLabel5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel5.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel5.setBounds(65, 211, 509, 53);
+		contentPane.add(lblNewLabel5);
+		
+		JLabel lblNewLabel6 = new JLabel(" - La feuille bat la pierre.");
+		lblNewLabel6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel6.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel6.setBounds(65, 253, 509, 53);
+		contentPane.add(lblNewLabel6);
+		
+		JButton btnQuitter = new JButton("Quitter");
+		btnQuitter.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnQuitter.setBounds(10, 329, 108, 39);
+		btnQuitter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				mainView frame = new mainView();
+			}
+		});
+		contentPane.add(btnQuitter);
+	}
+
+}
